5a4f5ab10663fe0150e5c86104c5926816b0f2cd
Omkatkar007 <omkatkar718@gmail.com>
2026-03-01 22:25:11 +0530

    update V0.5

--- script.js
+++ script.js
@@ -7,29 +7,202 @@
 const defaultProducts = [
     {
         id: 1,
-        name: "Banarasi Silk Saree",
+        name: "Sanskar sadi",
         image: "images/saree1.jpg",
-        price: 8500
+        price: 1250
     },
     {
         id: 2,
-        name: "Bindu",
-        image: "images/saree4.jpg",
-        price: 1550
+        name: "Ved’s rajhans silk",
+        image: "images/saree2.jpg",
+        price: 1650
     },
     {
         id: 3,
-        name: "Bandhani Saree",
+        name: "Ved’s rajhans silk",
         image: "images/saree3.jpg",
-        price: 6500
+        price: 1650
     },
     {
         id: 4,
-        name: "Chiffon Saree",
+        name: "Ved’s rajhans silk",
         image: "images/saree4.jpg",
-        price: 4500
+        price: 1650
+    
+    },
+    {
+        id: 5,
+        name: "Ved’s rajhans silk",
+        image: "images/saree5.jpg",
+        price: 1650
+    },
+    {
+        id: 6,
+        name: "Ved’s rajhans silk",
+        image: "images/saree6.jpg",
+        price: 1650
+    },
+    {
+        id: 7,
+        name: "Ved’s rajhans silk",
+        image: "images/saree7.jpg",
+        price: 1650
+    },
+    {
+        id: 8,
+        name: "Ved’s rajhans silk",
+        image: "images/saree8.jpg",
+        price: 1650
+    },
+    {
+        id: 9,
+        name: "Ved’s rajhans silk",
+        image: "images/saree9.jpg",
+        price: 1650
+    },
+    {
+        id: 10,
+        name: "Rajbanshi",
+        image: "images/saree10.jpg",
+        price: 1550
+    },
+    {
+        id: 11,
+        name: "Kalanjali Paithani with beads work blouse",
+        image: "images/saree11.jpg",
+        price: 1550
+    },
+    {
+        id: 12,
+        name: "blouse quality",
+        image: "images/saree12.jpg",
+        price: 1550
+    },
+        {
+            id: 13,
+            name: "kavita ",
+            image: "images/saree13.jpg",
+            price: 750
+        },
+    {   id: 14,
+        name: "MOSS FOIL SPECIAL BANDHANI ",
+        image: "images/saree14.jpg",
+        price: 750
+    },
+    {   id: 15,
+        name: "SPECIAL POUCH PACKING",
+        image: "images/saree15.jpg",
+        price: 750 
+    },
+    {   id: 16,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree16.jpg",
+        price: 600
+    },
+    {
+        id: 17,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree17.jpg",
+        price: 600
+    },
+    {
+        id: 18,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree18.jpg",
+        price: 600
+    },
+    {
+        id: 19,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree19.jpg",
+        price: 600
+    },
+    {
+        id: 20,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree20.jpg",
+        price: 600
+    },
+    {
+        id: 21,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree21.jpg",
+        price: 600
+    },
+    {
+        id: 22,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree22.jpg",
+        price: 600
+    },
+    {
+        id: 23,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree23.jpg",
+        price: 600
+    },
+    {
+        id: 24,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree24.jpg",
+        price: 600
+    },
+    {
+        id: 25,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree25.jpg",
+        price: 600
+    },
+    {
+        id: 26,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree26.jpg",
+        price: 600
+    },
+    {
+        id: 27,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree27.jpg",
+        price: 600
+    },
+    {
+        id: 28,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree28.jpg",
+        price: 600
+    },
+    {
+        id: 29,
+        name: "BOLLYWOOD cotton trending pattern",
+        image: "images/saree29.jpg",
+        price: 600
+    },
+    {
+
     }
-];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ];
+    
 
 // ===== DISPLAY PRODUCTS =====
 function displayProducts() {
